3616189013aeeb0558ad914871feb3fc83782c29
David Day <david.day@utexas.edu>
2021-11-09 22:08:47 -0600

    added remaining ports to gpio definitions

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,3 +1,6 @@
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
 enum PORT {
   PORTA,
   PORTB,
@@ -17,18 +20,17 @@ enum PIN {
   PIN7
 };
 
-//GPIO Definitions
-void PortF_Init(void); // prototype call
-
-void init_port_clock(enum PORT port);
-void init_pin_as_input(enum PORT port, enum PIN pin);
-void init_pin_as_output(enum PORT port, enum PIN pin);
-void init_pin_as_af(enum PORT port, enum PIN pin);
-void init_pin_as_gpio(enum PORT port, enum PIN pin);
-void set_pin_drive_strength_2(enum PORT port, enum PIN pin);
-void set_pin_drive_strength_4(enum PORT port, enum PIN pin);
-void set_pin_drive_strength_8(enum PORT port, enum PIN pin);
+void enable_port_clock(enum PORT port);
+void enable_pin_as_input(enum PORT port, enum PIN pin);
+void enable_pin_as_output(enum PORT port, enum PIN pin);
+void enable_pin_as_af(enum PORT port, enum PIN pin);
+void enable_pin_as_gpio(enum PORT port, enum PIN pin);
+void set_pin_drive_strength_2mA(enum PORT port, enum PIN pin);
+void set_pin_drive_strength_4mA(enum PORT port, enum PIN pin);
+void set_pin_drive_strength_8mA(enum PORT port, enum PIN pin);
 void enable_pin_digital(enum PORT port, enum PIN pin);
 void enable_pin_analog(enum PORT port, enum PIN pin);
 void set_gpio_output_pin_high(enum PORT port, enum PIN pin);
 void set_gpio_output_pin_low(enum PORT port, enum PIN pin);
+
+#endif /* _GPIO_H_ */
--- libs/gpio.c
+++ libs/gpio.c
@@ -3,7 +3,7 @@
 #include"gpio.h"
 #include "tm4c123gh6pm.h"
 
-struct GPIO_PORT {
+static struct GPIO_PORT {
   volatile uint32_t *DATA;
   volatile uint32_t *DIR;
   volatile uint32_t *IS;
@@ -35,54 +35,66 @@ static struct GPIO_PORT PORTS[] = {
   &GPIO_PORTA_RIS_R, &GPIO_PORTA_MIS_R, &GPIO_PORTA_ICR_R, &GPIO_PORTA_AFSEL_R, &GPIO_PORTA_DR2R_R, &GPIO_PORTA_DR4R_R,
   &GPIO_PORTA_DR8R_R, &GPIO_PORTA_ODR_R, &GPIO_PORTA_PUR_R, &GPIO_PORTA_PDR_R, &GPIO_PORTA_SLR_R, &GPIO_PORTA_DEN_R, 
   &GPIO_PORTA_LOCK_R, &GPIO_PORTA_CR_R, &GPIO_PORTA_AMSEL_R, &GPIO_PORTA_PCTL_R, &GPIO_PORTA_ADCCTL_R, &GPIO_PORTA_DMACTL_R},
+
+  {&GPIO_PORTB_DATA_R, &GPIO_PORTB_DIR_R, &GPIO_PORTB_IS_R, &GPIO_PORTB_IBE_R, &GPIO_PORTB_IEV_R, &GPIO_PORTB_IM_R,
+  &GPIO_PORTB_RIS_R, &GPIO_PORTB_MIS_R, &GPIO_PORTB_ICR_R, &GPIO_PORTB_AFSEL_R, &GPIO_PORTB_DR2R_R, &GPIO_PORTB_DR4R_R,
+  &GPIO_PORTB_DR8R_R, &GPIO_PORTB_ODR_R, &GPIO_PORTB_PUR_R, &GPIO_PORTB_PDR_R, &GPIO_PORTB_SLR_R, &GPIO_PORTB_DEN_R, 
+  &GPIO_PORTB_LOCK_R, &GPIO_PORTB_CR_R, &GPIO_PORTB_AMSEL_R, &GPIO_PORTB_PCTL_R, &GPIO_PORTB_ADCCTL_R, &GPIO_PORTB_DMACTL_R},
+
+  {&GPIO_PORTC_DATA_R, &GPIO_PORTC_DIR_R, &GPIO_PORTC_IS_R, &GPIO_PORTC_IBE_R, &GPIO_PORTC_IEV_R, &GPIO_PORTC_IM_R,
+  &GPIO_PORTC_RIS_R, &GPIO_PORTC_MIS_R, &GPIO_PORTC_ICR_R, &GPIO_PORTC_AFSEL_R, &GPIO_PORTC_DR2R_R, &GPIO_PORTC_DR4R_R,
+  &GPIO_PORTC_DR8R_R, &GPIO_PORTC_ODR_R, &GPIO_PORTC_PUR_R, &GPIO_PORTC_PDR_R, &GPIO_PORTC_SLR_R, &GPIO_PORTC_DEN_R, 
+  &GPIO_PORTC_LOCK_R, &GPIO_PORTC_CR_R, &GPIO_PORTC_AMSEL_R, &GPIO_PORTC_PCTL_R, &GPIO_PORTC_ADCCTL_R, &GPIO_PORTC_DMACTL_R},
+
+  {&GPIO_PORTD_DATA_R, &GPIO_PORTD_DIR_R, &GPIO_PORTD_IS_R, &GPIO_PORTD_IBE_R, &GPIO_PORTD_IEV_R, &GPIO_PORTD_IM_R,
+  &GPIO_PORTD_RIS_R, &GPIO_PORTD_MIS_R, &GPIO_PORTD_ICR_R, &GPIO_PORTD_AFSEL_R, &GPIO_PORTD_DR2R_R, &GPIO_PORTD_DR4R_R,
+  &GPIO_PORTD_DR8R_R, &GPIO_PORTD_ODR_R, &GPIO_PORTD_PUR_R, &GPIO_PORTD_PDR_R, &GPIO_PORTD_SLR_R, &GPIO_PORTD_DEN_R, 
+  &GPIO_PORTD_LOCK_R, &GPIO_PORTD_CR_R, &GPIO_PORTD_AMSEL_R, &GPIO_PORTD_PCTL_R, &GPIO_PORTD_ADCCTL_R, &GPIO_PORTD_DMACTL_R},
+
+  {&GPIO_PORTE_DATA_R, &GPIO_PORTE_DIR_R, &GPIO_PORTE_IS_R, &GPIO_PORTE_IBE_R, &GPIO_PORTE_IEV_R, &GPIO_PORTE_IM_R,
+  &GPIO_PORTE_RIS_R, &GPIO_PORTE_MIS_R, &GPIO_PORTE_ICR_R, &GPIO_PORTE_AFSEL_R, &GPIO_PORTE_DR2R_R, &GPIO_PORTE_DR4R_R,
+  &GPIO_PORTE_DR8R_R, &GPIO_PORTE_ODR_R, &GPIO_PORTE_PUR_R, &GPIO_PORTE_PDR_R, &GPIO_PORTE_SLR_R, &GPIO_PORTE_DEN_R, 
+  &GPIO_PORTE_LOCK_R, &GPIO_PORTE_CR_R, &GPIO_PORTE_AMSEL_R, &GPIO_PORTE_PCTL_R, &GPIO_PORTE_ADCCTL_R, &GPIO_PORTE_DMACTL_R},
+
+  {&GPIO_PORTF_DATA_R, &GPIO_PORTF_DIR_R, &GPIO_PORTF_IS_R, &GPIO_PORTF_IBE_R, &GPIO_PORTF_IEV_R, &GPIO_PORTF_IM_R,
+  &GPIO_PORTF_RIS_R, &GPIO_PORTF_MIS_R, &GPIO_PORTF_ICR_R, &GPIO_PORTF_AFSEL_R, &GPIO_PORTF_DR2R_R, &GPIO_PORTF_DR4R_R,
+  &GPIO_PORTF_DR8R_R, &GPIO_PORTF_ODR_R, &GPIO_PORTF_PUR_R, &GPIO_PORTF_PDR_R, &GPIO_PORTF_SLR_R, &GPIO_PORTF_DEN_R, 
+  &GPIO_PORTF_LOCK_R, &GPIO_PORTF_CR_R, &GPIO_PORTF_AMSEL_R, &GPIO_PORTF_PCTL_R, &GPIO_PORTF_ADCCTL_R, &GPIO_PORTF_DMACTL_R},
 };
 
-void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;     // 1) activate clock for Port F
-  delay = SYSCTL_RCGC2_R;           // allow time for clock to start
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on PF7-0
-  GPIO_PORTF_PUR_R = 0x11;          // enable pull-up on PF0 and PF4
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital I/O on PF4-0
-}
-
-void init_port_clock(enum PORT port) {
+void enable_port_clock(enum PORT port) {
   SYSCTL_RCGCGPIO_R |= 1 << port;
 }
 
-void init_pin_as_input(enum PORT port, enum PIN pin) {
+void enable_pin_as_input(enum PORT port, enum PIN pin) {
   *(PORTS[port].DIR) &= ~(1 << pin);
 }
 
-void init_pin_as_output(enum PORT port, enum PIN pin) {
+void enable_pin_as_output(enum PORT port, enum PIN pin) {
   *(PORTS[port].DIR) |= 1 << pin;
 }
 
-void init_pin_as_af(enum PORT port, enum PIN pin) {
+void enable_pin_as_af(enum PORT port, enum PIN pin) {
   *(PORTS[port].AFSEL) |= 1 << pin;
 }
 
-void init_pin_as_gpio(enum PORT port, enum PIN pin) {
+void enable_pin_as_gpio(enum PORT port, enum PIN pin) {
   *(PORTS[port].AFSEL) &= ~(1 << pin);
 }
 
-void set_pin_drive_strength_2(enum PORT port, enum PIN pin) {
+void set_pin_drive_strength_2mA(enum PORT port, enum PIN pin) {
   *(PORTS[port].DR2R) |= 1 << pin;
   *(PORTS[port].DR4R) &= ~(1 << pin);
   *(PORTS[port].DR8R) &= ~(1 << pin);
 }
 
-void set_pin_drive_strength_4(enum PORT port, enum PIN pin) {
+void set_pin_drive_strength_4mA(enum PORT port, enum PIN pin) {
   *(PORTS[port].DR2R) &= ~(1 << pin);
   *(PORTS[port].DR4R) |= 1 << pin;
   *(PORTS[port].DR8R) &= ~(1 << pin);
 }
 
-void set_pin_drive_strength_8(enum PORT port, enum PIN pin) {
+void set_pin_drive_strength_8mA(enum PORT port, enum PIN pin) {
   *(PORTS[port].DR2R) &= ~(1 << pin);
   *(PORTS[port].DR4R) &= ~(1 << pin);
   *(PORTS[port].DR8R) |= 1 << pin;
