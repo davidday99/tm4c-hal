fe4c32a7d14c15a032307083c757b830110d7a2d
David Day <david.day@utexas.edu>
2021-11-12 05:01:17 -0600

    added some simple code to main

--- src/main.c
+++ src/main.c
@@ -17,6 +17,34 @@ void Delay(void){unsigned long volatile time;
     }
 }
 
+char HEX_CONV[] = "abcdef";
+
+void hex_to_str(uint32_t val, char *buf) {
+    uint32_t i = 0;
+    char copy[20];
+    if (val == 0) {
+        copy[i++] = 0;
+    } else {
+        while (val > 0) {
+            uint8_t tmp = (val & 0xF);
+            if (tmp > 9) {
+                tmp = HEX_CONV[tmp - 10];
+            } else {
+                tmp += '0';
+            }
+            copy[i++] = tmp;
+            val >>= 4;
+        }
+    }
+    uint32_t j = 2;
+    buf[0] = '0';
+    buf[1] = 'x';
+    while (i > 0) {
+        buf[j++] = copy[--i];
+    }
+    buf[j] = '\0';
+}
+
 int main(void){
     uint8_t delay = 0;
 
@@ -24,44 +52,18 @@ int main(void){
 
     PLL_init();
 
-    // enable SSI0
-    //SYSCTL_RCGCSSI_R |= 1;
-
-    /**
-    * Enable the following pins:
-    * PA2 = SSI0Clk
-    * PA3 = SSI0Fss
-    * PA4 = SSI0Rx
-    * PA5 = SSI0Tx
-    **/
-    // enable PA clock
-    // init_port_clock(PORTA);
-    // delay++;
-    // init_pin_as_output(PORTA, PIN2);
-    // enable_pin_digital(PORTA, PIN2);
-
-
-    // set_gpio_output_pin_high(PORTA, PIN2);
-    // Delay();
-    // set_gpio_output_pin_low(PORTA, PIN2);
-
-    //init_SSI(0, 1, 100, 0, 0, 8);
-
     Output_Init();
     ST7735_OutString("Hello, world!\n");
     Delay();
-    ST7735_OutString("Checking diagnostics\n");
-    Delay();
+
+    ST7735_OutString("Address of i:\n");
+    char buf[20];
+    hex_to_str((uint32_t) &buf, buf);
+    ST7735_OutString(buf);
+    ST7735_OutString("\n");
 
     while (1) {
-        ST7735_OutString(".");
-        Delay();
-        ST7735_OutString(".");
-        Delay();
-        ST7735_OutString(".");
-        Delay();
-        ST7735_OutChar(8);
-        ST7735_OutChar(8);
-        ST7735_OutChar(8);
+
     }
+
 }
