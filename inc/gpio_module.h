ab7fd6df504d703ad5f3f4304933b69bc29b403e
David Day <david.day@utexas.edu>
2021-11-12 03:54:22 -0600

    created low-level GPIO API above registers

---
+++ inc/gpio_module.h
@@ -0,0 +1,79 @@
+#ifndef _GPIO_MODULE_H_
+#define _GPIO_MODULE_H_
+
+extern struct GPIO_PIN PORTA_PIN0;
+extern struct GPIO_PIN PORTA_PIN1;
+extern struct GPIO_PIN PORTA_PIN2;
+extern struct GPIO_PIN PORTA_PIN3;
+extern struct GPIO_PIN PORTA_PIN4;
+extern struct GPIO_PIN PORTA_PIN5;
+extern struct GPIO_PIN PORTA_PIN6;
+extern struct GPIO_PIN PORTA_PIN7;
+
+extern struct GPIO_PIN PORTB_PIN0;
+extern struct GPIO_PIN PORTB_PIN1;
+extern struct GPIO_PIN PORTB_PIN2;
+extern struct GPIO_PIN PORTB_PIN3;
+extern struct GPIO_PIN PORTB_PIN4;
+extern struct GPIO_PIN PORTB_PIN5;
+extern struct GPIO_PIN PORTB_PIN6;
+extern struct GPIO_PIN PORTB_PIN7;
+
+extern struct GPIO_PIN PORTC_PIN0;
+extern struct GPIO_PIN PORTC_PIN1;
+extern struct GPIO_PIN PORTC_PIN2;
+extern struct GPIO_PIN PORTC_PIN3;
+extern struct GPIO_PIN PORTC_PIN4;
+extern struct GPIO_PIN PORTC_PIN5;
+extern struct GPIO_PIN PORTC_PIN6;
+extern struct GPIO_PIN PORTC_PIN7;
+
+extern struct GPIO_PIN PORTD_PIN0;
+extern struct GPIO_PIN PORTD_PIN1;
+extern struct GPIO_PIN PORTD_PIN2;
+extern struct GPIO_PIN PORTD_PIN3;
+extern struct GPIO_PIN PORTD_PIN4;
+extern struct GPIO_PIN PORTD_PIN5;
+extern struct GPIO_PIN PORTD_PIN6;
+extern struct GPIO_PIN PORTD_PIN7;
+
+extern struct GPIO_PIN PORTE_PIN0;
+extern struct GPIO_PIN PORTE_PIN1;
+extern struct GPIO_PIN PORTE_PIN2;
+extern struct GPIO_PIN PORTE_PIN3;
+extern struct GPIO_PIN PORTE_PIN4;
+extern struct GPIO_PIN PORTE_PIN5;
+extern struct GPIO_PIN PORTE_PIN6;
+extern struct GPIO_PIN PORTE_PIN7;
+
+extern struct GPIO_PIN PORTF_PIN0;
+extern struct GPIO_PIN PORTF_PIN1;
+extern struct GPIO_PIN PORTF_PIN2;
+extern struct GPIO_PIN PORTF_PIN3;
+extern struct GPIO_PIN PORTF_PIN4;
+extern struct GPIO_PIN PORTF_PIN5;
+extern struct GPIO_PIN PORTF_PIN6;
+extern struct GPIO_PIN PORTF_PIN7;
+
+enum DRIVE_STRENGTH {
+    DS_2ma,
+    DS_4ma,
+    DS_8ma
+};
+
+void init_gpio_port_clock(struct GPIO_PIN *gpio_pin);
+void init_gpio_pin_as_input(struct GPIO_PIN *gpio_pin);
+void init_gpio_pin_as_output(struct GPIO_PIN *gpio_pin);
+void init_gpio_pin_as_digital(struct GPIO_PIN *gpio_pin);
+void init_gpio_pin_as_analog(struct GPIO_PIN *gpio_pin);
+void enable_gpio_pin_alternate_function(struct GPIO_PIN *gpio_pin);
+void disable_gpio_pin_alternate_function(struct GPIO_PIN *gpio_pin);
+void set_gpio_pin_drive_strength(struct GPIO_PIN *gpio_pin, enum DRIVE_STRENGTH drive_strength);
+void enable_gpio_pin_pullup_resistor(struct GPIO_PIN *gpio_pin);
+void enable_gpio_pin_pulldown_resistor(struct GPIO_PIN *gpio_pin);
+void set_gpio_pin_alternate_function(struct GPIO_PIN *gpio_pin, uint32_t value);
+void unlock_and_commit_gpio_pin(struct GPIO_PIN *gpio_pin);
+void uncommit_and_lock_gpio_pin(struct GPIO_PIN *gpio_pin);
+
+
+#endif /* _GPIO_MODULE_H_ */
---
+++ libs/modules/gpio_module.c
@@ -0,0 +1,137 @@
+#include <stdint.h>
+#include "gpio_module.h"
+#include "gpio.h"
+#include "common.h"
+#include "system.h"
+
+struct GPIO_PIN PORTA_PIN0 = {PORTA, PIN0};
+struct GPIO_PIN PORTA_PIN1 = {PORTA, PIN1};
+struct GPIO_PIN PORTA_PIN2 = {PORTA, PIN2};
+struct GPIO_PIN PORTA_PIN3 = {PORTA, PIN3};
+struct GPIO_PIN PORTA_PIN4 = {PORTA, PIN4};
+struct GPIO_PIN PORTA_PIN5 = {PORTA, PIN5};
+struct GPIO_PIN PORTA_PIN6 = {PORTA, PIN6};
+struct GPIO_PIN PORTA_PIN7 = {PORTA, PIN7};
+
+struct GPIO_PIN PORTB_PIN0 = {PORTB, PIN0};
+struct GPIO_PIN PORTB_PIN1 = {PORTB, PIN1};
+struct GPIO_PIN PORTB_PIN2 = {PORTB, PIN2};
+struct GPIO_PIN PORTB_PIN3 = {PORTB, PIN3};
+struct GPIO_PIN PORTB_PIN4 = {PORTB, PIN4};
+struct GPIO_PIN PORTB_PIN5 = {PORTB, PIN5};
+struct GPIO_PIN PORTB_PIN6 = {PORTB, PIN6};
+struct GPIO_PIN PORTB_PIN7 = {PORTB, PIN7};
+
+struct GPIO_PIN PORTC_PIN0 = {PORTC, PIN0};
+struct GPIO_PIN PORTC_PIN1 = {PORTC, PIN1};
+struct GPIO_PIN PORTC_PIN2 = {PORTC, PIN2};
+struct GPIO_PIN PORTC_PIN3 = {PORTC, PIN3};
+struct GPIO_PIN PORTC_PIN4 = {PORTC, PIN4};
+struct GPIO_PIN PORTC_PIN5 = {PORTC, PIN5};
+struct GPIO_PIN PORTC_PIN6 = {PORTC, PIN6};
+struct GPIO_PIN PORTC_PIN7 = {PORTC, PIN7};
+
+struct GPIO_PIN PORTD_PIN0 = {PORTD, PIN0};
+struct GPIO_PIN PORTD_PIN1 = {PORTD, PIN1};
+struct GPIO_PIN PORTD_PIN2 = {PORTD, PIN2};
+struct GPIO_PIN PORTD_PIN3 = {PORTD, PIN3};
+struct GPIO_PIN PORTD_PIN4 = {PORTD, PIN4};
+struct GPIO_PIN PORTD_PIN5 = {PORTD, PIN5};
+struct GPIO_PIN PORTD_PIN6 = {PORTD, PIN6};
+struct GPIO_PIN PORTD_PIN7 = {PORTD, PIN7};
+
+struct GPIO_PIN PORTE_PIN0 = {PORTE, PIN0};
+struct GPIO_PIN PORTE_PIN1 = {PORTE, PIN1};
+struct GPIO_PIN PORTE_PIN2 = {PORTE, PIN2};
+struct GPIO_PIN PORTE_PIN3 = {PORTE, PIN3};
+struct GPIO_PIN PORTE_PIN4 = {PORTE, PIN4};
+struct GPIO_PIN PORTE_PIN5 = {PORTE, PIN5};
+struct GPIO_PIN PORTE_PIN6 = {PORTE, PIN6};
+struct GPIO_PIN PORTE_PIN7 = {PORTE, PIN7};
+
+struct GPIO_PIN PORTF_PIN0 = {PORTF, PIN0};
+struct GPIO_PIN PORTF_PIN1 = {PORTF, PIN1};
+struct GPIO_PIN PORTF_PIN2 = {PORTF, PIN2};
+struct GPIO_PIN PORTF_PIN3 = {PORTF, PIN3};
+struct GPIO_PIN PORTF_PIN4 = {PORTF, PIN4};
+struct GPIO_PIN PORTF_PIN5 = {PORTF, PIN5};
+struct GPIO_PIN PORTF_PIN6 = {PORTF, PIN6};
+struct GPIO_PIN PORTF_PIN7 = {PORTF, PIN7};
+
+static const uint32_t GPIO_LOCK = 0x4C4F434B;
+
+void init_gpio_port_clock(struct GPIO_PIN *gpio_pin) {
+    set_RCGCGPIO(gpio_pin->port, 1);
+}
+
+void init_gpio_pin_as_input(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_DIR(gpio_pin->port, gpio_pin->pin, 0);
+}
+
+void init_gpio_pin_as_output(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_DIR(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void init_gpio_pin_as_digital(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_AMSEL(gpio_pin->port, gpio_pin->pin, 0);
+    init_gpio_pin_DEN(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void init_gpio_pin_as_analog(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_DEN(gpio_pin->port, gpio_pin->pin, 0);
+    init_gpio_pin_AMSEL(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void enable_gpio_pin_alternate_function(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_AFSEL(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void disable_gpio_pin_alternate_function(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_AFSEL(gpio_pin->port, gpio_pin->pin, 0);
+}
+
+void set_gpio_pin_drive_strength(struct GPIO_PIN *gpio_pin, enum DRIVE_STRENGTH drive_strength) {
+    switch (drive_strength) {
+        case DS_2ma:
+            init_gpio_pin_DR4R(gpio_pin->port, gpio_pin->pin, 0);
+            init_gpio_pin_DR8R(gpio_pin->port, gpio_pin->pin, 0);
+            init_gpio_pin_DR2R(gpio_pin->port, gpio_pin->pin, 1);
+            break;
+        case DS_4ma:
+            init_gpio_pin_DR2R(gpio_pin->port, gpio_pin->pin, 0);
+            init_gpio_pin_DR8R(gpio_pin->port, gpio_pin->pin, 0);
+            init_gpio_pin_DR4R(gpio_pin->port, gpio_pin->pin, 1);
+            break;
+        case DS_8ma:
+            init_gpio_pin_DR2R(gpio_pin->port, gpio_pin->pin, 0);
+            init_gpio_pin_DR4R(gpio_pin->port, gpio_pin->pin, 0);
+            init_gpio_pin_DR8R(gpio_pin->port, gpio_pin->pin, 1);
+            break;
+    }
+}
+
+void enable_gpio_pin_pullup_resistor(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_PDR(gpio_pin->port, gpio_pin->pin, 0);
+    init_gpio_pin_PUR(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void enable_gpio_pin_pulldown_resistor(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_PUR(gpio_pin->port, gpio_pin->pin, 0);
+    init_gpio_pin_PDR(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void set_gpio_pin_alternate_function(struct GPIO_PIN *gpio_pin, uint32_t value) {
+    init_gpio_pin_PCTL(gpio_pin->port, gpio_pin->pin, value);
+}
+
+void unlock_and_commit_gpio_pin(struct GPIO_PIN *gpio_pin) {
+    unlock_gpio_port(gpio_pin->port);
+    init_gpio_pin_CR(gpio_pin->port, gpio_pin->pin, 1);
+}
+
+void uncommit_and_lock_gpio_pin(struct GPIO_PIN *gpio_pin) {
+    init_gpio_pin_CR(gpio_pin->port, gpio_pin->pin, 0);
+    lock_gpio_port(gpio_pin->port);
+}
+
+
