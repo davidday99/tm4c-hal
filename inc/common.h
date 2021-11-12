2753d419d3ed4e0fc06e713eabc7de696c591433
David Day <david.day@utexas.edu>
2021-11-11 23:07:33 -0600

    moved reused enums to common header

---
+++ inc/common.h
@@ -0,0 +1,23 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+enum PORT {
+    PORTA,
+    PORTB,
+    PORTC,
+    PORTD,
+    PORTF
+};
+
+enum PIN {
+    PIN0,
+    PIN1,
+    PIN2,
+    PIN3,
+    PIN4,
+    PIN5,
+    PIN6,
+    PIN7
+};
+
+#endif /* _COMMON_H_ */
