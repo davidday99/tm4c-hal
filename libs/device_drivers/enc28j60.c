0c503d8169e22c28a1c8a97d3a6f1035c034a763
David Day <david.day@utexas.edu>
2021-11-16 19:52:00 -0600

    got bit_field_set/clear working, removed duplicate macros, fixed ESTAT macro typo

--- libs/device_drivers/enc28j60.c
+++ libs/device_drivers/enc28j60.c
@@ -42,37 +42,7 @@
 #define EDMACSH 0x17
 #define EIE 0x1B
 #define EIR 0x1C
-#define ESTAT 0xD
-#define ECON2 0x1E
-#define ECON1 0x1F
-
-#define ERDPTL 0
-#define ERDPTH 0x01
-#define EWRPTL 0x02
-#define EWRPTH 0x03
-#define ETXSTL 0x04
-#define ETXSTH 0x05
-#define ETXNDL 0x06
-#define ETXNDH 0x07
-#define ERXSTL 0x08
-#define ERXSTH 0x09
-#define ERXNDL 0xA
-#define ERXNDH 0xB
-#define ERXRDPTL 0xC
-#define ERXRDPTH 0xD
-#define ERXWRPTL 0xE
-#define ERXWRPTH 0xF
-#define EDMASTL 0x10
-#define EDMASTH 0x11
-#define EDMANDL 0x12
-#define EDMANDH 0x13
-#define EDMADSTL 0x14
-#define EDMADSTH 0x15
-#define EDMACSL 0x16
-#define EDMACSH 0x17
-#define EIE 0x1B
-#define EIR 0x1C
-#define ESTAT 0xD
+#define ESTAT 0x1D
 #define ECON2 0x1E
 #define ECON1 0x1F
 
@@ -148,25 +118,27 @@ void delay_50ns(uint32_t n) {
     }
 }
 
-static void read_control_register(struct ENC28J60 *enc28j60, uint8_t reg, uint16_t *data) {
+static uint8_t read_control_register(struct ENC28J60 *enc28j60, uint8_t reg) {
     reg = (reg & 0x1F) | RCR_OPCODE;
     uint16_t dummy[2];
     dummy[0] = (uint16_t) reg;
     dummy[1] = 0;
+    uint16_t data[2];
     set_gpio_pin_low(enc28j60->cs);
     write_ssi(enc28j60->ssi, dummy, 2);
     read_ssi(enc28j60->ssi, data, 2);
     set_gpio_pin_high(enc28j60->cs);
+    return data[1];
 }
 
 static void read_buffer_memory(struct ENC28J60 *enc28j60, uint16_t *data, uint8_t bytes) {
     uint8_t cmd = RBM_OPCODE | RBM_ARG0;
     uint16_t dummy[bytes + 1];
     dummy[0] = (uint16_t) cmd;
-    set_gpio_pin_low(enc28j60->ssi->fss);
+    set_gpio_pin_low(enc28j60->cs);
     write_ssi(enc28j60->ssi, dummy, bytes + 1);
     read_ssi(enc28j60->ssi, data, bytes);
-    set_gpio_pin_high(enc28j60->ssi->fss);
+    set_gpio_pin_high(enc28j60->cs);
 }
 
 static void write_control_register(struct ENC28J60 *enc28j60, uint8_t reg, uint16_t data) {
@@ -188,10 +160,10 @@ static void write_buffer_memory(struct ENC28J60 *enc28j60, uint16_t *data, uint8
     uint8_t cmd = WBM_OPCODE | WBM_ARG0;
     uint16_t dummy[bytes + 1];
     dummy[0] = (uint16_t) cmd;
-    set_gpio_pin_low(enc28j60->ssi->fss);
+    set_gpio_pin_low(enc28j60->cs);
     write_ssi(enc28j60->ssi, dummy, 1);
     write_ssi(enc28j60->ssi, data, bytes);
-    set_gpio_pin_high(enc28j60->ssi->fss);
+    set_gpio_pin_high(enc28j60->cs);
 }
 
 static void bit_field_set(struct ENC28J60 *enc28j60, uint8_t reg, uint8_t bitfield) {
@@ -199,9 +171,13 @@ static void bit_field_set(struct ENC28J60 *enc28j60, uint8_t reg, uint8_t bitfie
     uint16_t data[2];
     data[0] = (uint16_t) reg;
     data[1] = (uint16_t) bitfield;
-    set_gpio_pin_low(enc28j60->ssi->fss);
+    set_gpio_pin_low(enc28j60->cs);
     write_ssi(enc28j60->ssi, data, 2);
-    set_gpio_pin_high(enc28j60->ssi->fss);
+    while (ssi_is_busy(enc28j60->ssi))
+        ;
+    set_gpio_pin_high(enc28j60->cs);
+    while (!ssi_rx_empty(enc28j60->ssi)) 
+        read_ssi(enc28j60->ssi, data, 1);
 }
 
 static void bit_field_clear(struct ENC28J60 *enc28j60, uint8_t reg, uint8_t bitfield) {
@@ -209,17 +185,21 @@ static void bit_field_clear(struct ENC28J60 *enc28j60, uint8_t reg, uint8_t bitf
     uint16_t data[2];
     data[0] = (uint16_t) reg;
     data[1] = (uint16_t) bitfield;
-    set_gpio_pin_low(enc28j60->ssi->fss);
+    set_gpio_pin_low(enc28j60->cs);
     write_ssi(enc28j60->ssi, data, 2);
-    set_gpio_pin_high(enc28j60->ssi->fss);
+    while (ssi_is_busy(enc28j60->ssi))
+        ;
+    set_gpio_pin_high(enc28j60->cs);
+    while (!ssi_rx_empty(enc28j60->ssi)) 
+        read_ssi(enc28j60->ssi, data, 1);
 }
 
 static void system_reset(struct ENC28J60 *enc28j60) {
     uint8_t cmd = SRC_OPCODE | SRC_ARG0;
     uint16_t cmd16 = (uint16_t) cmd;
-    set_gpio_pin_low(enc28j60->ssi->fss);
+    set_gpio_pin_low(enc28j60->cs);
     write_ssi(enc28j60->ssi, &cmd16, 1);
-    set_gpio_pin_high(enc28j60->ssi->fss);
+    set_gpio_pin_high(enc28j60->cs);
 }
 
 static void ENC28J60_init_peripherals(struct ENC28J60 *enc28j60) {
@@ -235,22 +215,29 @@ static void ENC28J60_init_peripherals(struct ENC28J60 *enc28j60) {
     set_gpio_pin_high(enc28j60->cs);
 }
 
+static void init_rx_buffer(struct ENC28J60 *enc28j60) {
+    write_control_register(enc28j60, ERXSTL, 0);  // Rx buffer start
+    write_control_register(enc28j60, ERXSTH, 0);
+    write_control_register(enc28j60, ERXNDL, 0xFE);  // Rx buffer end
+    write_control_register(enc28j60, ERXNDH, 0x0F);
+    write_control_register(enc28j60, ERXRDPTL, 0);  // set rx read ptr to start; this is only for tracking
+    write_control_register(enc28j60, ERXRDPTH, 0);
+}
+
+static void init_receive_filters(struct ENC28J60 *enc28j60) {
+    write_control_register(enc28j60, ECON1, 0x01); // switch to bank 1
+    write_control_register(enc28j60, ERXFCON, 0);  // enable promiscuous mode (receive all packets)
+}
+
 uint8_t ENC28J60_init(struct ENC28J60 *enc28j60) {
     ENC28J60_init_peripherals(enc28j60);
-    uint16_t data = 0x03;
-    uint16_t read[4] = {0};
-    uint32_t i = 0;
-    // while (read[0] == 0) {
-    //     write_control_register(enc28j60, ECON2, &data, 1);
-    //     read_control_register(enc28j60, ECON2, read, 4);
-    //     i++;
-    // }
-    //
-    write_control_register(enc28j60, ECON1, data);
+    init_rx_buffer(enc28j60);
+    init_receive_filters(enc28j60);
 
-    read_control_register(enc28j60, ECON1, read);
-    // init_rx_buf(enc28j60);
-    return read[1];
+    while ((read_control_register(enc28j60, ESTAT) & 1) == 0)  // wait for OST
+        ;
+
+    return read_control_register(enc28j60, ESTAT);
 }
 
 void ENC28J60_read_data(struct ENC28J60 *enc28j60, uint8_t data[], uint32_t size) {
@@ -258,9 +245,5 @@ void ENC28J60_read_data(struct ENC28J60 *enc28j60, uint8_t data[], uint32_t size
 }
 
 void ENC28J60_write_data(struct ENC28J60 *enc28j60, uint8_t *data, uint32_t size) {
-    return;
-}
-
-void init_rx_buf(struct ENC28J60 *enc28j60) {
     return;
 }
