4c62adc9a4a5c587db63cb05e29198d91172e953
David Day <david.day@utexas.edu>
2021-11-30 13:54:56 -0600

    updated connections comment

--- libs/device_drivers/st7735.c
+++ libs/device_drivers/st7735.c
@@ -45,14 +45,12 @@
 // hardware connections
 // **********ST7735 TFT and SDC*******************
 // ST7735
-// Backlight (pin 10) connected to +3.3 V
-// MISO (pin 9) unconnected
-// SCK (pin 8) connected to PA2 (SSI0Clk)
-// MOSI (pin 7) connected to PA5 (SSI0Tx)
-// TFT_CS (pin 6) connected to PA3 (SSI0Fss)
-// CARD_CS (pin 5) unconnected
-// Data/Command (pin 4) connected to PA6 (GPIO), high for data, low for command
-// RESET (pin 3) connected to PA7 (GPIO)
+// LED+ connected to +3.3 V
+// CS connected to PA3 (SSI0Fss)
+// SCL conected to PA2 (SSI0Clk)
+// SDA connected to PA5 (SSI0Tx)
+// A0 connected to PA6 (GPIO), high for data, low for command
+// RESET conntected to PA7 (GPIO)
 // VCC (pin 2) connected to +3.3 V
 // Gnd (pin 1) connected to ground
 
