7e71d9dc97ee33cb1bdd3f5e999f79051dd3ad14
David Day <david.day@utexas.edu>
2021-12-01 12:48:06 -0600

    removed outdated comment

--- libs/device_drivers/st7735.c
+++ libs/device_drivers/st7735.c
@@ -161,19 +161,6 @@
 
 #define DELAY 0x80
 
-/**
- * ST7735
- * Backlight (pin 10) connected to +3.3 V
- * MISO (pin 9) unconnected
- * SCK (pin 8) connected to PA2 (SSI0Clk)
- * MOSI (pin 7) connected to PA5 (SSI0Tx)
- * TFT_CS (pin 6) connected to PA3 (SSI0Fss)
- * CARD_CS (pin 5) unconnected
- * Data/Command (pin 4) connected to PA6 (GPIO), high for data, low for command
- * RESET (pin 3) connected to PA7 (GPIO)
- * VCC (pin 2) connected to +3.3 V
- * Gnd (pin 1) connected to ground
- */
 struct ST7735 ST7735 = {
     &SSI_0,
     &PORTA_PIN6,
