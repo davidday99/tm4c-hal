e63b36cc7721333a4a62ca7a8e2e34a0cf01b2ad
David Day <david.day@utexas.edu>
2021-12-11 16:05:14 -0600

    Change parameters of enc to interface better with ethernet

--- inc/enc.h
+++ inc/enc.h
@@ -4,13 +4,21 @@
 #include <stdint.h>
 #include "ethernet.h"
 
+#define ENC_RX_BUF_LEN 1518
+#define ENC_TX_BUF_LEN 1518
+
 typedef struct ENC {
     void *model;
 } ENC;
 
+extern uint8_t enc_rx_buffer[ENC_RX_BUF_LEN];
+extern uint8_t enc_tx_buffer[ENC_TX_BUF_LEN];
+
+extern uint8_t enc_frame_waiting;
+
 uint8_t enc_init(ENC *enc);
-void enc_write_frame(ENC *enc, struct enet_frame *e);
-void enc_read_frame(ENC *enc, struct enet_frame *e);
+void enc_write_frame(ENC *enc, uint8_t *data, uint16_t len);
+uint16_t enc_read_frame(ENC *enc);
 uint32_t enc_get_mac_address_low(ENC *enc);
 uint32_t enc_get_mac_address_high(ENC *enc);
 #endif /* _ENC_H_ */
--- libs/devices/enc.c
+++ libs/devices/enc.c
@@ -3,20 +3,26 @@
 #include "ethernet.h"
 #include "enc28j60.h"
 
+uint8_t enc_rx_buffer[ENC_RX_BUF_LEN];
+uint8_t enc_tx_buffer[ENC_TX_BUF_LEN];
+
+uint8_t enc_frame_waiting;
+uint8_t enc_frame_len;
+
 uint8_t enc_init(ENC *enc) {
     enc->model = &ENC28J60;
     return ENC28J60_init(enc->model);
 }
 
-void enc_write_frame(ENC *enc, struct enet_frame *e) {
-    uint8_t *data = (uint8_t*) e;
-    ENC28J60_write_frame(enc->model, data, e->dlen + 18);
+void enc_write_frame(ENC *enc, uint8_t *data, uint16_t len) {
+    ENC28J60_write_frame(enc->model, data, len);
 }
 
-void enc_read_frame(ENC *enc, struct enet_frame *e) {
+uint16_t enc_read_frame(ENC *enc) {
     uint8_t data[1518];
-    uint16_t len = ENC28J60_read_frame(enc->model, data);
-    init_frame(e, data, len);
+    uint16_t len = ENC28J60_read_frame(enc->model, enc_rx_buffer);
+    enc_frame_waiting = 1;
+    return len;
 }
 
 uint32_t enc_get_mac_address_low(ENC *enc) {
