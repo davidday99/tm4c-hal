bce42acb837d94d12ddaeec0de513dc8f67695aa
David Day <david.day@utexas.edu>
2021-12-11 16:06:03 -0600

    added functions for printing ethernet frame and arp packet, changed main loop to read frame from enc28j60 into enc buffer

--- src/main.c
+++ src/main.c
@@ -6,8 +6,10 @@
 #include "enc28j60.h"
 #include "enc.h"
 #include "ethernet.h"
-#include "ip.h"
 #include "lcd.h"
+#include "netcommon.h"
+#include "arp.h"
+#include "ip_util.h"
 
 extern void EnableInterrupts();
 extern void DisableInterrupts();
@@ -47,6 +49,54 @@ uint32_t strlen(const char *s) {
     return len;
 }
 
+void print_ethernet_frame(LCD *lcd, uint8_t *data, uint16_t len) {
+    struct enethdr *hdr = (struct enethdr *) data;
+
+    lcd_write(lcd, "des:");
+    for (uint8_t i = 0; i < ENET_HEADER_DEST_LEN; i++)
+        lcd_write(lcd, "%x%c", hdr->dest[i], i < ENET_HEADER_DEST_LEN - 1 ? '-' : ' ');
+    lcd_write(lcd, "\n");
+
+    lcd_write(lcd, "src:");
+    for (uint8_t i = 0; i < ENET_HEADER_SRC_LEN; i++)
+        lcd_write(lcd, "%x%c", hdr->src[i], i < ENET_HEADER_SRC_LEN - 1 ? '-' : ' ');
+    lcd_write(lcd, "\n");
+
+    lcd_write(lcd, "type:0x%X\n", hton16(hdr->type));
+
+    lcd_write(lcd, "frame size:%d\n", len);    
+}
+
+void print_arp_packet(LCD *lcd, struct arphdr *hdr) {
+    char ipaddr[16];
+    lcd_write(lcd, "HW Type: %d\n", hton16(hdr->hwtype));
+    lcd_write(lcd, "Pcol Type: 0x%X\n", hton16(hdr->ptype));
+    lcd_write(lcd, "HW Len: %d\n", hdr->hwlen);
+    lcd_write(lcd, "Pcol Len: %d\n", hdr->plen);
+    lcd_write(lcd, "Opcode: %d\n", hton16(hdr->opcode));
+
+    int_to_ipv4(hton32(hdr->psender), ipaddr);
+    lcd_write(lcd, "Sender IP:\n");
+    lcd_write(lcd, ipaddr);
+    lcd_write(lcd, "\n");
+
+
+    lcd_write(lcd, "Sender MAC:\n");
+    for (uint8_t i = 0; i < ENET_HEADER_DEST_LEN; i++)
+        lcd_write(lcd, "%x%c", hdr->hwsender[i], i < ENET_HEADER_DEST_LEN - 1 ? '-' : ' ');
+    lcd_write(lcd, "\n");
+
+    int_to_ipv4(hton32(hdr->ptarget), ipaddr);
+    lcd_write(lcd, "Target IP:\n");
+    lcd_write(lcd, ipaddr);
+    lcd_write(lcd, "\n");
+
+    lcd_write(lcd, "Target MAC:\n");
+    for (uint8_t i = 0; i < ENET_HEADER_DEST_LEN; i++)
+        lcd_write(lcd, "%x%c", hdr->hwtarget[i], i < ENET_HEADER_DEST_LEN - 1 ? '-' : ' ');
+    lcd_write(lcd, "\n");  
+}
+
 int main(void){
     uint8_t delay = 0;
 
@@ -54,18 +104,11 @@ int main(void){
 
     PLL_init();
 
-    struct enet_frame e_frame;
-    struct ip_pkt ip;
-    uint32_t len;
-
-
     LCD lcd;
     ENC enc;
     lcd_init(&lcd);
-
-    uint8_t read = enc_init(&enc);
     
-    if (read) {
+    if (enc_init(&enc)) {
         lcd_write(&lcd, "ENC initialized.\n");
         lcd_write(&lcd, "Enabling receive.\n");
         ENC28J60_enable_receive(&ENC28J60) ? lcd_write(&lcd, "Receive enabled.\n") : 
@@ -76,29 +119,17 @@ int main(void){
 
     lcd_write(&lcd, "Waiting for frames.\n");
     
-    uint8_t prev = 0;
-    read = 0;
+    uint8_t pktcnt = 0;
+    uint16_t len;
     while (1) {
-        // ENC28J60_write_frame(&ENC28J60, transmit, 16);
-        // read += 1;
-        read = get_packet_count(&ENC28J60);
-        if (prev != read) {
-            prev = read;
-            enc_read_frame(&enc, &e_frame);
-            
-            lcd_write(&lcd, "des:");
-            for (uint8_t i = 0; i < ENET_DEST_LEN; i++)
-                lcd_write(&lcd, "%x%c", e_frame.dest[i], i < ENET_DEST_LEN - 1 ? '-' : ' ');
-            lcd_write(&lcd, "\n");
-
-            lcd_write(&lcd, "src:");
-            for (uint8_t i = 0; i < ENET_SRC_LEN; i++)
-                lcd_write(&lcd, "%x%c", e_frame.src[i], i < ENET_SRC_LEN - 1 ? '-' : ' ');
-            lcd_write(&lcd, "\n");
-
-            lcd_write(&lcd, "type:0x%X\n", e_frame.type);
-
-            lcd_write(&lcd, "frame size:%d\n", e_frame.dlen);            
+        Delay1s();
+        pktcnt = get_packet_count(&ENC28J60);
+        if (pktcnt > 0) {
+            len = enc_read_frame(&enc);
+            // ethernet_handle_frame(enc_rx_buffer);
+            // print_ethernet_frame(&lcd, enc_rx_buffer, len);    
+            if (hton16(((struct enethdr *) enc_rx_buffer)->type) == ETHERTYPE_ARP)
+                print_arp_packet(&lcd, (struct arphdr *) (enc_rx_buffer + ENET_DATA_OFFSET));
         }
     }
 }
