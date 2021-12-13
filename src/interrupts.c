bc91f07da34757273cb493edd38c8e0103a30b61
David Day <david.day@utexas.edu>
2021-12-13 03:21:26 -0600

    added mediocre ISR for PB1 falling edge

--- src/interrupts.c
+++ src/interrupts.c
@@ -1,4 +1,7 @@
 #include <stdint.h>
+#include "event_queue.h"
+#include "gpio.h"
+#include "common.h"
 
 void BusFault_Handler(void) {
     while (1)
@@ -8,8 +11,11 @@ void BusFault_Handler(void) {
 }
 
 void GPIOPortB_ISR(void) {
-    while (1)
-    {
+    uint8_t pin = get_gpio_port_MIS(PORTB);
+    set_gpio_pin_ICR(PORTB, 1);
+    set_gpio_pin_IM(PORTB, 1, 0);
+    if (pin == 2)
+        event_queue_push(EVENT_ETHERNET_RECEIVE);
 
-    }
+    set_gpio_pin_IM(PORTB, 1, 1);
 }
