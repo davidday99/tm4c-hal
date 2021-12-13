6de9b6583b4f286c4c585fca815598eb3435866f
David Day <david.day@utexas.edu>
2021-12-13 03:23:10 -0600

    create library for stdlib functions until I figure out how to link against the one in arm-gcc

---
+++ inc/stdlib.h
@@ -0,0 +1,8 @@
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
+
+#include <stdint.h>
+
+void memcopy(void *dest, const void *src, uint32_t bytes);
+
+#endif /* _STDLIB_H_ */
---
+++ libs/stdlib/stdlib.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+void memcopy(void *dest, const void *src, uint32_t bytes) {
+    for (uint32_t i = 0; i < bytes; i++)
+        *((uint8_t *) dest++) = *((uint8_t *) src++);
+}
+
