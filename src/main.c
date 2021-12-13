d26cc18dcdbc7e61c62ce0962a15f3afd83b70cd
David Day <david.day@utexas.edu>
2021-12-13 03:20:54 -0600

    restructured with event queue

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include "netcommon.h"
 #include "arp.h"
 #include "ip_util.h"
+#include "stdlib.h"
+#include "event_queue.h"
 
 extern void EnableInterrupts();
 extern void DisableInterrupts();
@@ -42,12 +44,12 @@ uint8_t transmit[16] = {
     0xFF
 };
 
-uint32_t strlen(const char *s) {
-    uint32_t len = 0;
-    while (*s++)
-        len++;
-    return len;
-}
+// uint32_t strlen(const char *s) {
+//     uint32_t len = 0;
+//     while (*s++)
+//         len++;
+//     return len;
+// }
 
 void print_ethernet_frame(LCD *lcd, uint8_t *data, uint16_t len) {
     struct enethdr *hdr = (struct enethdr *) data;
@@ -65,36 +67,6 @@ void print_ethernet_frame(LCD *lcd, uint8_t *data, uint16_t len) {
     lcd_write(lcd, "type:0x%X\n", hton16(hdr->type));
 
     lcd_write(lcd, "frame size:%d\n", len);    
-}
-
-void print_arp_packet(LCD *lcd, struct arphdr *hdr) {
-    char ipaddr[16];
-    lcd_write(lcd, "HW Type: %d\n", hton16(hdr->hwtype));
-    lcd_write(lcd, "Pcol Type: 0x%X\n", hton16(hdr->ptype));
-    lcd_write(lcd, "HW Len: %d\n", hdr->hwlen);
-    lcd_write(lcd, "Pcol Len: %d\n", hdr->plen);
-    lcd_write(lcd, "Opcode: %d\n", hton16(hdr->opcode));
-
-    int_to_ipv4(hton32(hdr->psender), ipaddr);
-    lcd_write(lcd, "Sender IP:\n");
-    lcd_write(lcd, ipaddr);
-    lcd_write(lcd, "\n");
-
-
-    lcd_write(lcd, "Sender MAC:\n");
-    for (uint8_t i = 0; i < ENET_HEADER_DEST_LEN; i++)
-        lcd_write(lcd, "%x%c", hdr->hwsender[i], i < ENET_HEADER_DEST_LEN - 1 ? '-' : ' ');
-    lcd_write(lcd, "\n");
-
-    int_to_ipv4(hton32(hdr->ptarget), ipaddr);
-    lcd_write(lcd, "Target IP:\n");
-    lcd_write(lcd, ipaddr);
-    lcd_write(lcd, "\n");
-
-    lcd_write(lcd, "Target MAC:\n");
-    for (uint8_t i = 0; i < ENET_HEADER_DEST_LEN; i++)
-        lcd_write(lcd, "%x%c", hdr->hwtarget[i], i < ENET_HEADER_DEST_LEN - 1 ? '-' : ' ');
-    lcd_write(lcd, "\n");  
 }
 
 int main(void){
@@ -117,19 +89,12 @@ int main(void){
         lcd_write(&lcd, "Could not init.\n");
     }
 
-    lcd_write(&lcd, "Waiting for frames.\n");
-    
-    uint8_t pktcnt = 0;
-    uint16_t len;
+    EnableInterrupts();
+
     while (1) {
-        Delay1s();
-        pktcnt = get_packet_count(&ENC28J60);
-        if (pktcnt > 0) {
-            len = enc_read_frame(&enc);
-            // ethernet_handle_frame(enc_rx_buffer);
-            // print_ethernet_frame(&lcd, enc_rx_buffer, len);    
-            if (hton16(((struct enethdr *) enc_rx_buffer)->type) == ETHERTYPE_ARP)
-                print_arp_packet(&lcd, (struct arphdr *) (enc_rx_buffer + ENET_DATA_OFFSET));
-        }
+        event_t e = event_queue_pop();
+        if (e == EVENT_QUEUE_EMPTY)
+            continue;
+        event_queue_handle_event(e, &lcd, &enc);
     }
 }
