ae2833eda9d4e199483602fda3ff54f2bfa0111d
David Day <david.day@utexas.edu>
2021-12-13 19:00:18 -0600

    Add rmemcopy to copy src in reverse, not actually used anymore

--- inc/stdlib.h
+++ inc/stdlib.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 void memcopy(void *dest, const void *src, uint32_t bytes);
+void rmemcopy(void *dest, const void *src, uint32_t bytes);
 
 #endif /* _STDLIB_H_ */
--- libs/stdlib/stdlib.c
+++ libs/stdlib/stdlib.c
@@ -5,3 +5,7 @@ void memcopy(void *dest, const void *src, uint32_t bytes) {
         *((uint8_t *) dest++) = *((uint8_t *) src++);
 }
 
+void rmemcopy(void *dest, const void *src, uint32_t bytes) {
+    for (uint32_t i = 0; i < bytes; i++)
+        *((uint8_t *) dest++) = *((uint8_t *) src--);
+}
