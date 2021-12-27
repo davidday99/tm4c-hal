eb0a3490d0a0008b99c3fd31137b40420c782b13
David Day <david.day@utexas.edu>
2021-12-26 18:56:18 -0600

    remove custom stdlib and use existing one

--- inc/stdlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STDLIB_H_
-#define _STDLIB_H_
-
-#include <stdint.h>
-
-void memcopy(void *dest, const void *src, uint32_t bytes);
-void rmemcopy(void *dest, const void *src, uint32_t bytes);
-
-#endif /* _STDLIB_H_ */
