3c37f70a5848f293582c38d7b796b14243a4f263
David Day <david.day@utexas.edu>
2022-01-02 21:04:43 -0600

    test udp rx

--- test/testrunner.c
+++ test/testrunner.c
@@ -15,18 +15,18 @@ extern void DisableInterrupts();
 extern void StartCritical();
 extern void EndCritical();
 
-int test1() {
-    return 1;
-}
-
 int test_arp_send_many();
+int test_rx_udp_packet();
 
 int (*test_cases[])(void) = {
-    test_arp_send_many
+    test_arp_send_many,
+    test_rx_udp_packet
 };
 
 LCD lcd;
-ENC enc;
+ENC _enc;
+ENC *enc = &_enc;
+
 
 int main(void){
 
@@ -35,8 +35,8 @@ int main(void){
     PLL_init();
 
     lcd_init(&lcd);
-    enc_init(&enc);
-    enc_enable(&enc);
+    enc_init(enc);
+    enc_enable(enc);
 
     EnableInterrupts();
 
@@ -47,6 +47,8 @@ int main(void){
     for (unsigned int i = 0; i < test_count; i++) {
         if (test_cases[i]() == 0) {
             lcd_write(&lcd, "TEST %d FAILED!\n", i);
+        } else {
+            lcd_write(&lcd, "Test %d passed.\n", i);
         }
     }
 
