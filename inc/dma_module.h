9b7d4c135079dcffa3dd8c8f4860f898bae836f0
David Day <david.day@utexas.edu>
2022-01-10 19:01:50 -0600

    raise up interrupt functions from low-level API, redesign DMA object to fit existing HAL design

--- inc/dma_module.h
+++ inc/dma_module.h
@@ -4,18 +4,53 @@
 #include <stdint.h>
 #include "common.h"
 
-struct DMA_MODULE {
+struct DMA {
     enum DMA_CHANNEL channel;
 };
 
-void init_dma(struct DMA_MODULE *dma, enum DMA_CHANNEL ch, uint8_t source);
-void start_dma_transfer_mem(struct DMA_MODULE *dma, uint8_t *srcendptr, 
+extern struct DMA DMA_CH0;
+extern struct DMA DMA_CH1;
+extern struct DMA DMA_CH2;
+extern struct DMA DMA_CH3;
+extern struct DMA DMA_CH4;
+extern struct DMA DMA_CH5;
+extern struct DMA DMA_CH6;
+extern struct DMA DMA_CH7;
+extern struct DMA DMA_CH8;
+extern struct DMA DMA_CH9;
+extern struct DMA DMA_CH10;
+extern struct DMA DMA_CH11;
+extern struct DMA DMA_CH12;
+extern struct DMA DMA_CH13;
+extern struct DMA DMA_CH14;
+extern struct DMA DMA_CH15;
+extern struct DMA DMA_CH16;
+extern struct DMA DMA_CH17;
+extern struct DMA DMA_CH18;
+extern struct DMA DMA_CH19;
+extern struct DMA DMA_CH20;
+extern struct DMA DMA_CH21;
+extern struct DMA DMA_CH22;
+extern struct DMA DMA_CH23;
+extern struct DMA DMA_CH24;
+extern struct DMA DMA_CH25;
+extern struct DMA DMA_CH26;
+extern struct DMA DMA_CH27;
+extern struct DMA DMA_CH28;
+extern struct DMA DMA_CH29;
+extern struct DMA DMA_CH30;
+extern struct DMA DMA_CH31;
+
+void init_dma(struct DMA *dma, uint8_t source);
+void start_dma_transfer_mem(struct DMA *dma, uint8_t *srcendptr, 
                         uint8_t *destendptr, uint16_t size);
-void start_dma_transfer_peripheral_tx(struct DMA_MODULE *dma, uint8_t *srcendptr, 
+void start_dma_transfer_peripheral_tx(struct DMA *dma, uint8_t *srcendptr, 
                         uint8_t *destendptr, uint16_t size);
-void start_dma_transfer_peripheral_rx(struct DMA_MODULE *dma, uint8_t *srcendptr, 
+void start_dma_transfer_peripheral_rx(struct DMA *dma, uint8_t *srcendptr, 
                         uint8_t *destendptr, uint16_t size);
-uint16_t remaining_dma_transfer_count(struct DMA_MODULE *dma);
+uint16_t remaining_dma_transfer_count(struct DMA *dma);
 uint8_t dma_busy(void);
+uint8_t dma_interrupt_occurred(struct DMA *dma);
+void clear_dma_interrupt(struct DMA *dma);
 
 #endif /* _DMA_H_ */
--- lib/hal/modules/dma_module.c
+++ lib/hal/modules/dma_module.c
@@ -3,14 +3,46 @@
 #include "common.h"
 #include "system.h"
 
-void init_dma(struct DMA_MODULE *dma, enum DMA_CHANNEL ch, uint8_t source) {
-    dma->channel = ch;
+struct DMA DMA_CH0 = {DMACH0};
+struct DMA DMA_CH1 = {DMACH1};
+struct DMA DMA_CH2 = {DMACH2};
+struct DMA DMA_CH3 = {DMACH3};
+struct DMA DMA_CH4 = {DMACH4};
+struct DMA DMA_CH5 = {DMACH5};
+struct DMA DMA_CH6 = {DMACH6};
+struct DMA DMA_CH7 = {DMACH7};
+struct DMA DMA_CH8 = {DMACH8};
+struct DMA DMA_CH9 = {DMACH9};
+struct DMA DMA_CH10 = {DMACH10};
+struct DMA DMA_CH11 = {DMACH11};
+struct DMA DMA_CH12 = {DMACH12};
+struct DMA DMA_CH13 = {DMACH13};
+struct DMA DMA_CH14 = {DMACH14};
+struct DMA DMA_CH15 = {DMACH15};
+struct DMA DMA_CH16 = {DMACH16};
+struct DMA DMA_CH17 = {DMACH17};
+struct DMA DMA_CH18 = {DMACH18};
+struct DMA DMA_CH19 = {DMACH19};
+struct DMA DMA_CH20 = {DMACH20};
+struct DMA DMA_CH21 = {DMACH21};
+struct DMA DMA_CH22 = {DMACH22};
+struct DMA DMA_CH23 = {DMACH23};
+struct DMA DMA_CH24 = {DMACH24};
+struct DMA DMA_CH25 = {DMACH25};
+struct DMA DMA_CH26 = {DMACH26};
+struct DMA DMA_CH27 = {DMACH27};
+struct DMA DMA_CH28 = {DMACH28};
+struct DMA DMA_CH29 = {DMACH29};
+struct DMA DMA_CH30 = {DMACH30};
+struct DMA DMA_CH31 = {DMACH31};
+
+void init_dma(struct DMA *dma, uint8_t source) {
     set_RCGCDMA();
     init_DMACHn(dma->channel, 0);
     set_DMACHn_channel_source(dma->channel, source);
 }
 
-void start_dma_transfer_mem(struct DMA_MODULE *dma, uint8_t *srcendptr, 
+void start_dma_transfer_mem(struct DMA *dma, uint8_t *srcendptr, 
                         uint8_t *destendptr, uint16_t size) {
     set_DMACHn_src_end(dma->channel, srcendptr);
     set_DMACHn_dest_end(dma->channel, destendptr);
@@ -21,7 +53,7 @@ void start_dma_transfer_mem(struct DMA_MODULE *dma, uint8_t *srcendptr,
     issue_DMACHn_software_request(dma->channel);
 }
 
-void start_dma_transfer_peripheral_tx(struct DMA_MODULE *dma, uint8_t *srcendptr, 
+void start_dma_transfer_peripheral_tx(struct DMA *dma, uint8_t *srcendptr, 
                         uint8_t *destendptr, uint16_t size) {
     set_DMACHn_src_end(dma->channel, srcendptr);
     set_DMACHn_dest_end(dma->channel, destendptr);
@@ -32,7 +64,7 @@ void start_dma_transfer_peripheral_tx(struct DMA_MODULE *dma, uint8_t *srcendptr
     enable_DMACHn(dma->channel);
 }
 
-void start_dma_transfer_peripheral_rx(struct DMA_MODULE *dma, uint8_t *srcendptr, 
+void start_dma_transfer_peripheral_rx(struct DMA *dma, uint8_t *srcendptr, 
                         uint8_t *destendptr, uint16_t size) {
     set_DMACHn_src_end(dma->channel, srcendptr);
     set_DMACHn_dest_end(dma->channel, destendptr);
@@ -43,7 +75,7 @@ void start_dma_transfer_peripheral_rx(struct DMA_MODULE *dma, uint8_t *srcendptr
     enable_DMACHn(dma->channel);
 }
 
-uint16_t remaining_dma_transfer_count(struct DMA_MODULE *dma) {
+uint16_t remaining_dma_transfer_count(struct DMA *dma) {
     struct DMA_CONTROL_STRUCTURE_T *cs = get_DMACHn_control_structure(dma->channel);
     return (cs->ctrlword >> 4) & 0x3FF;
 }
@@ -54,4 +86,12 @@ uint8_t dma_busy(void) {
         return 0;
     else
         return 1;
+}
+
+uint8_t dma_interrupt_occurred(struct DMA *dma) {
+    return get_DMACHn_interrupt_status(dma->channel);
+}
+
+void clear_dma_interrupt(struct DMA *dma) {
+    clear_DMACHn_interrupt(dma->channel);
 }
